f87cf2138d6377713fc8412d358646a891492aa8
Rafael <rafamoresi@gmail.com>
2022-12-18 20:42:14 -0300

    Apply SRP on Model

---
+++ Model/Concessionaria.cs
@@ -0,0 +1,26 @@
+namespace ApiVeiculos.Model
+{
+    public class Concessionaria
+    {
+        public string Nome { get; private set; }
+        public string Endereco { get; private set; }
+
+        public void SetNome(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public void SetEndereco(string endereco)
+        {
+            this.Endereco = endereco;
+        }
+
+        public bool VerificaPrazoRevisao(Carro carro)
+        {
+            if (DateTime.Now.Date > carro.DataRevisao)
+                return true;
+
+            return false;
+        }
+    }
+}
