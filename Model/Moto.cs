7049961487b07353b6d086773b09a23b72e78d67
Rafael <rafamoresi@gmail.com>
2022-12-18 14:36:26 -0300

    Add Model Encapsulation

--- Model/Moto.cs
+++ Model/Moto.cs
@@ -2,9 +2,49 @@ namespace ApiVeiculos.Model
 {
     public class Moto
     {
-        public int MotoId { get; set; }
-        public string Placa { get; set; }
-        public string Cor { get; set; }
-        public string Fabricante { get; set; }
+        public int MotoId { get; private set; }
+        public string Placa { get; private set; }
+        public string Cor { get; private set; }
+        public string Fabricante { get; private set; }
+        public int Quilometros { get; private set; }
+        public DateTime DataRevisao { get; private set; }
+
+        public void SetPlaca(string placa)
+        {
+            this.Placa = placa;
+        }
+
+        public void SetCor(string cor)
+        {
+            this.Cor = cor;
+        }
+
+        public void SetFabricante(string fabricante)
+        {
+            this.Fabricante = fabricante;
+        }
+
+        public void SetQuilometros(int quilometros)
+        {
+            this.Quilometros = quilometros;
+        }
+
+        public void SetDataRevisao(DateTime dataRevisao)
+        {
+            this.DataRevisao = dataRevisao;
+        }
+
+        public double QuilometragemPorLitro(int quantidadeCombustivel)
+        {
+            return this.Quilometros / quantidadeCombustivel;
+        }
+
+        public bool VerificaPrazoRevisao()
+        {
+            if (DateTime.Now.Date > DataRevisao)
+                return true;
+
+            return false;
+        }
     }
 }
