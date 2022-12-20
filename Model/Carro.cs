f98a6807c1dca40af9ea8f086c3137c89b24fde5
Rafael <rafamoresi@gmail.com>
2022-12-20 16:23:16 -0300

    Entity implements interface

--- Model/Carro.cs
+++ Model/Carro.cs
@@ -1,7 +1,11 @@
+using ApiVeiculos.Model.Interfaces;
+
 namespace ApiVeiculos.Model
 {
-    public class Carro : VeiculoMotorizado
+    public class Carro : VeiculoMotorizado, IRegistravel
     {
         public int QuantidadeLugares { get; set; }
+        public string Placa { get; set; }
+        public DateTime DataRevisao { get; set; }
     }
 }
