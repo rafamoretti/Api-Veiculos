bc79550c49bfcc015b854787d99aab6bed418cc0
Rafael <rafamoresi@gmail.com>
2022-12-20 22:04:31 -0300

    Add Services

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using ApiVeiculos.Context;
+using ApiVeiculos.Repository;
+using ApiVeiculos.Repository.Interfaces;
+using ApiVeiculos.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +15,11 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<ICarroRepository, CarroRepository>();
+builder.Services.AddScoped<IMotoRepository, MotoRepository>();
+builder.Services.AddScoped<IVeiculosMotorizadosService, VeiculosMotorizadosService>();
+builder.Services.AddScoped<IVeiculosRegistraveisService, VeiculosRegistraveisService>();
 
 var app = builder.Build();
 
